b962d2dbb54deda40e1165799bc3b67ffd3dbe30
vananh30 <s3836320@rmit.edu.vn>
2021-11-23 15:18:16 +0700

    Van anh

--- Login-UI-UX-React-Native-main/App.js
+++ Login-UI-UX-React-Native-main/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Login from './screens/Login';
 import Signup from './screens/Signup';
+import Welcome from './screens/Welcome';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -14,8 +15,9 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}} >
-      <Stack.Screen name="Login" component={Login} />
+     <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 }
