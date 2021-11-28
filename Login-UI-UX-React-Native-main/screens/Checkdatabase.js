3e53a0ad29f94b135847962af2ff2a8aaf322594
Nguyen The Minh <s3878434@rmit.edu.vn>
2021-11-28 14:00:51 +0700

    Finish the first check database page

--- Login-UI-UX-React-Native-main/screens/Checkdatabase.js
+++ Login-UI-UX-React-Native-main/screens/Checkdatabase.js
@@ -6,120 +6,119 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { alignContent, flex, flexDirection, style, width } from 'styled-system';
 import { withTheme } from 'styled-components';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 
 function Checkdatabase() {
     const navigation = useNavigation();
   return (
+    // body
     <SafeAreaView style={styles.grid_container}>
-      <View style = {styles.container}>
-        <View style={styles.leftside}>
-          <Text style = {styles.header}>Vaccheck</Text>
+      <View style = {styles.body}>
+        <View style={styles.container}>
+          {/* header */}
+          <View style={styles.leftside}>
+            <Text style = {styles.header}>Vaccheck</Text>
+          </View>
+          {/* request */}
+          <View style={styles.rightside}>
+            <Image style = {styles.request} source={require('../assets/Homepage/request.png')} />  
+          </View>
         </View>
-        <View style={styles.rightside}>
-          <Image style = {styles.request} source={require('../assets/Homepage/request.png')} />  
+        <View style = {styles.return_container}>
+          <Image style = {styles.back} source ={require('../assets/Checkdatabase/BackArrow.png')}></Image>
         </View>
-      </View>
-      <View style = {styles.return_container}>
-        <Image style = {styles.back} source ={require('../assets/Checkdatabase/BackArrow.png')}></Image>
-      </View>
-      {/* category */}
-      <View>
-        <Text style = {styles.type}>Student</Text>
-      </View>
-      {/* search */}
-      <View style = {styles.search_container}>
-        <View style={[styles.search, styles.container]}>
-          <Image style = {styles.search_icon} source ={require('../assets/Checkdatabase/Search.png')}></Image>
-          <Text style = {styles.search_letter}>Search</Text>
+        {/* category */}
+        <View>
+          <Text style = {styles.type}>Student</Text>
         </View>
-      </View>
-      {/* category selection */}
-      <View style = {styles.category}>
-        <View style = {[styles.category_box, {backgroundColor: '#46FA63'}]}>
-          <Text>Pass</Text>
+        {/* search */}
+        <View style = {styles.search_container}>
+          <View style={[styles.search, styles.container]}>
+            <Image style = {styles.search_icon} source ={require('../assets/Checkdatabase/Search.png')}></Image>
+            <Text style = {styles.search_letter}>Search</Text>
+          </View>
         </View>
-        <View style = {[styles.category_box, {backgroundColor: '#FF5A5A'}]}>
-          <Text>Fail</Text>
+        {/* category selection */}
+        <View style = {styles.category}>
+          <View style = {[styles.category_box, {backgroundColor: '#46FA63'}]}>
+            <Text>Pass</Text>
+          </View>
+          <View style = {[styles.category_box, {backgroundColor: '#FF5A5A'}]}>
+            <Text>Fail</Text>
+          </View>
+          <View style = {[styles.category_box, {backgroundColor: '#46D9FA'}]}>
+            <Text>All</Text>
+          </View>
         </View>
-        <View style = {[styles.category_box, {backgroundColor: '#46D9FA'}]}>
-          <Text>All</Text>
+        {/* list */}
+        <View style = {styles.list}>
+          <View style = {styles.list_header_section}>
+            <Text style = {styles.list_header}>
+              A
+            </Text>
+          </View>
+          <View style = {styles.list_content}>
+            <Text style = {styles.student_identity}>
+              Huynh Van Anh
+            </Text>
+            <Text style = {styles.student_identity}>
+              Nguyen Viet Anh
+            </Text>
+            <Text style = {styles.student_identity}>
+              Nguyen Duc Anh
+            </Text>
+            <Text style = {styles.student_identity}>
+              Phan Duc Anh
+            </Text>
+          </View>
+          <View style = {styles.list_header_section}>
+            <Text style = {styles.list_header}>
+              B
+            </Text>
+          </View>
+          <View style = {styles.list_content}>
+            <Text style = {styles.student_identity}>
+              Nguyen Duc Bao
+            </Text>
+            <Text style = {styles.student_identity}>
+              Pham Duc Binh
+            </Text>
+            <Text style = {styles.student_identity}>
+              Pham Minh Binh
+            </Text>
+          </View>
+          <View style = {styles.list_header_section}>
+            <Text style = {styles.list_header}>
+              C
+            </Text>
+          </View>
+          <View style = {styles.list_content}>
+            <Text style = {styles.student_identity}>
+              Nguyen Chi Cong
+            </Text>
+            <Text style = {styles.student_identity}>
+              Pham Duc Cong
+            </Text>
+          </View>
         </View>
-      </View>
-      {/* list */}
-      <View style = {styles.list}>
-        <View style = {styles.list_header_section}>
-          <Text style = {styles.list_header}>
-            A
-          </Text>
-        </View>
-        <View style = {styles.list_content}>
-          <Text style = {styles.student_identity}>
-            Huynh Van Anh
-          </Text>
-          <Text style = {styles.student_identity}>
-            Nguyen Viet Anh
-          </Text>
-          <Text style = {styles.student_identity}>
-            Nguyen Duc Anh
-          </Text>
-          <Text style = {styles.student_identity}>
-            Phan Duc Anh
-          </Text>
-        </View>
-        <View style = {styles.list_header_section}>
-          <Text style = {styles.list_header}>
-            B
-          </Text>
-        </View>
-        <View style = {styles.list_content}>
-          <Text style = {styles.student_identity}>
-            Nguyen Duc Bao
-          </Text>
-          <Text style = {styles.student_identity}>
-            Pham Duc Binh
-          </Text>
-        </View>
-        <View style = {styles.list_header_section}>
-          <Text style = {styles.list_header}>
-            C
-          </Text>
-        </View>
-        <View style = {styles.list_content}>
-          <Text style = {styles.student_identity}>
-            Nguyen Chi Cong
-          </Text>
-          <Text style = {styles.student_identity}>
-            Pham Duc Cong
-          </Text>
-        </View>
-        <View style = {styles.search_by_container}>
-        <Text style = {styles.search_text}>
-          Search by
-        </Text>
-      </View>
       </View>
       {/* search by section */}
-      
-      
-      {/* social media section */}
-      {/* <View style = {styles.social_media_section_container}>
-        <View style = {styles.social_media_container}>
-          <Image style = {styles.social_media_icon} source={require('../assets/Homepage/Discord.png')} />  
+      <View style = {styles.search_by_container}>
+        <Text style = {styles.search_text}>
+          Search by: 
+        </Text>
+        <View style = {styles.search_by_category_container}>
+          <View style = {styles.search_by_category}>
+            <Text> Student's name</Text>
+          </View>
+          <View style = {styles.search_by_category}>
+            <Text> Student's id</Text>
+          </View>
         </View>
-        <View style = {styles.social_media_container}>
-          <Image style = {styles.social_media_icon} source={require('../assets/Homepage/facebook.png')} />  
-        </View>
-        <View style = {styles.social_media_container}>
-          <Image style = {styles.social_media_icon} source={require('../assets/Homepage/instagram.png')} />  
-        </View>
-        <View style = {styles.social_media_container}>
-          <Image style = {styles.social_media_icon} source={require('../assets/Homepage/twitter.png')} />  
-        </View>
-      </View> */}
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
-    
   );
 }
 
@@ -135,12 +134,15 @@ export default () => {
 
 const styles = StyleSheet.create({
   grid_container: {
-    flex: 1,
+    flex: 10,
     flexDirection: "column",
     backgroundColor: '#fff',
   },
   container: {
     flexDirection: "row",
+  },
+  body: {
+    flex: 9,
   },
   middle: {
     alignItems: 'center',
@@ -162,84 +164,6 @@ const styles = StyleSheet.create({
   },
   request: {
     marginRight: 30,
-  },
-  menu: {
-    flexDirection: "column",
-    backgroundColor: "rgba(28, 119, 195, 0.5)",
-    borderRadius: 25,
-    marginTop: 54,
-    height: "55%",
-  },
-  introduction: {
-    fontWeight: "bold",
-    fontSize: 14,
-    marginTop: 30,
-    textAlign: "center",
-    marginLeft: 9,
-    marginRight: 9,
-  },
-  social_media_section_container: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-    marginBottom: 30,
-  },
-  social_media_container: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    backgroundColor: "#CBCBCB",
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 10,
-  },
-  social_media_icon: {
-    width: 45,
-    height: 45,
-  },
-  first_row: {
-    borderBottomColor: "black",
-    borderBottomWidth: 2,
-    height: "45%",
-    marginLeft: 20,
-    marginRight: 20,
-    alignItems: 'flex-end',
-  },
-  second_row: {
-    borderTopColor: "black",
-    borderTopWidth: 2,
-    height: "45%",
-    marginLeft: 20,
-    marginRight: 20,
-    alignItems: 'flex-start',
-  },
-  leftcolumn: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRightWidth: 2,
-    borderRightColor: "black",
-    paddingTop: "15%",
-    paddingBottom: "15%",
-  },
-  rightcolumn: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderLeftWidth: 2,
-    borderLeftColor: "black",
-    paddingTop: "15%",
-    paddingBottom: "15%",
-  },
-  function_icon: {
-    width: 50,
-    height: 50,
-  },
-  text: {
-    fontSize: 12,
   },
   return_container: {
     marginTop: 25,
@@ -315,12 +239,22 @@ const styles = StyleSheet.create({
   search_by_container: {
     backgroundColor: "#717171",
     flexDirection: 'column',
-    flex: 3,
+    height: "100%",
+    flex: 1,
   },
   search_text: {
     padding: 15,
-    color: "blue",
-
-  }
-  
+    color: "white",
+  },
+  search_by_category_container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  search_by_category: {
+    borderRadius: 5,
+    width: 150,
+    height: '100%',
+    backgroundColor: "white",
+    padding: 5,
+  },
 });
